3b45edfc4114ba8f4d845eab1733a680eb5eedcb
wataflai <anthonygenio2005@gmail.com>
2021-06-26 16:14:10 -0400

    begun trying to integrate the calculus thing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+from forms import CalculusForm
+from calc import Polynomial
 import os
 
 app = Flask(__name__)
@@ -14,12 +16,26 @@ for pic in os.listdir('./static/pics'):
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('home.html', title='pics', things=['this', 'is', 'a', 'rendered', 'list', 'hehe'], pics=pics)
 
 @app.route('/hehe')
 def hehe():
     return '<h1>hehe</h1>'
+
+@app.route('/calc')
+def calc():
+    form = CalculusForm()
+    if form.validate_on_submit():
+        poly = Polynomial(degree=int(form.degree), coefficients=list(map(int, form.coeffs.split())))
+        results = {
+            'evaluated at point' : enumerate([poly.eval(i) for i in range(11)]),
+            'derivative/slope at point' : enumerate([poly.derivative(i) for i in range(11)]),
+            'area under 0 to 10': poly.area_under(0, 10)
+
+        }
+    return render_template('calc.html', title='calc', form=form)
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
