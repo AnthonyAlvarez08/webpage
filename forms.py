98a53d33aeaea902ca2a38e2d3e08630200da802
wataflai <anthonygenio2005@gmail.com>
2021-06-27 11:01:11 -0400

    the input fields show up, idk how to recieve it

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class CalculusForm(FlaskForm):
 
-    degree = StringField('degree', validators=[DataRequired()])
-    coeffs = StringField('coeffs', validators=[DataRequired()])
-    submit = SubmitField('come up')
+    degree = IntegerField('degree', validators=[DataRequired(), Length(min=1)])
+    coeffs = StringField('coeffs', validators=[DataRequired(), Length(min=1)])
+    submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from forms import CalculusForm
 from calc import Polynomial
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'njvaovnfohandvfliansvdjf161f1bg64s1bs345fv1s6'
 
 # define the port and host just incase I change it in the future, for now localhost 5000
 HOST = '127.0.0.1'
 PORT = 5000
 
 pics = list()
+results = dict()  # global for helping and doddo
 
 for pic in os.listdir('./static/pics'):
     pics.append(pic) 
@@ -24,18 +26,35 @@ def home():
 def hehe():
     return '<h1>hehe</h1>'
 
-@app.route('/calc')
+
+@app.route('/res', methods=['GET', 'POST'])
+def res():
+    global results
+    temp = list()
+    for i, b in results:
+        temp.append(f'{i} : {str(b)}')
+    return render_template('calcRes.html', title='res', results=temp)
+
+@app.route('/calc', methods=['GET', 'POST'])
 def calc():
+    global results
     form = CalculusForm()
+    print('here')
     if form.validate_on_submit():
-        poly = Polynomial(degree=int(form.degree), coefficients=list(map(int, form.coeffs.split())))
+        print('here3')
+        poly = Polynomial(degree=form.degree.data, coefficients=list(map(int, form.coeffs.data.split())))
         results = {
             'evaluated at point' : enumerate([poly.eval(i) for i in range(11)]),
             'derivative/slope at point' : enumerate([poly.derivative(i) for i in range(11)]),
             'area under 0 to 10': poly.area_under(0, 10)
 
         }
+        print(results)
+        return redirect(url_for('res'))
+    print('here2')
     return render_template('calc.html', title='calc', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
